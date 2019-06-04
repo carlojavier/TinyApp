0ea295d893e0a423ca0a28963a5f3d0a644bd79f
Carlo <carlomjavier@gmail.com>
2019-06-04 22:07:12 +0000

    printed URLs

--- express_server.js
+++ express_server.js
@@ -2,15 +2,16 @@ var express = require("express");
 var app = express();
 var PORT = 8080;
 
-app.set("view enging", "ejs");
+app.set("view engine", "ejs");
 
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
 };
 
-app.get("/Dracarys", (require, response) => {
-    response.send("<html><body>Dracarys <b>World</b></body></html>\n");
+app.get("/urls", (require, response) => {
+    let templateVars = { urls: urlDatabase };
+    response.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {
