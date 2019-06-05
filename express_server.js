ac72193823bca299a05e9c3cd94a2e4ce9dfcdd7
Carlo <carlomjavier@gmail.com>
2019-06-05 02:05:14 +0000

    got the url form working!

--- express_server.js
+++ express_server.js
@@ -9,9 +9,16 @@ const urlDatabase = {
     "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/urls", (request, response) => {
     let templateVars = { urls: urlDatabase };
     response.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (request, response) => {
+    response.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (request, response) => {
@@ -20,6 +27,15 @@ app.get("/urls/:shortURL", (request, response) => {
     response.render("urls_show", templateVars);
 });
 
+app.post("/urls", (request, response) => {
+    console.log(req.body); // Log the POST request body to the console
+    response.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
     console.log(`Sample app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+
+}
